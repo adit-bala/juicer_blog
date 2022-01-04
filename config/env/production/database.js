3ed1fff662dfe7be696bc9d38357b8c730d93531
Adit Bala <adityarbala@gmail.com>
2022-01-03 20:45:40 -0800

    add Cloudinary support :pray:

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,32 +1,28 @@
-const parse = require('pg-connection-string').parse;
-    const config = parse(process.env.DATABASE_URL);
-    module.exports = ({ env }) => ({
-      upload: {
-        config: {
-          provider: 'cloudinary',
-          providerOptions: {
-            cloud_name: env('CLOUDINARY_NAME'),
-            api_key: env('CLOUDINARY_KEY'),
-            api_secret: env('CLOUDINARY_SECRET'),
-          },
-          actionOptions: {
-            upload: {},
-            delete: {},
-          },
-        },
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          user: config.user,
-          password: config.password,
-          ssl: {
-            rejectUnauthorized: false
-          },
-        },
-        debug: false,
+    },
+  },
+  connection: {
+    client: "postgres",
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false,
       },
-    });
+    },
+    debug: false,
+  },
+});
