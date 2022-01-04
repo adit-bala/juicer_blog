3607b7bd36c24826c934d1c67910d40d89c96b94
Adit Bala <adityarbala@gmail.com>
2022-01-03 20:30:26 -0800

    attempt to add image support

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,6 +1,20 @@
 const parse = require('pg-connection-string').parse;
     const config = parse(process.env.DATABASE_URL);
     module.exports = ({ env }) => ({
+      upload: {
+        config: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+          },
+          actionOptions: {
+            upload: {},
+            delete: {},
+          },
+        },
+      },
       connection: {
         client: 'postgres',
         connection: {
